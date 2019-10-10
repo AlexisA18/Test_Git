2776ee8fec7a478b15654e04d76af46f032719cc
nxf46116 <nxf46116@NXL12621.wbi.nxp.com>
2019-10-10 15:22:25 -0500

    Test 2

--- source/Test_Git.c
+++ source/Test_Git.c
@@ -56,7 +56,7 @@ int main(void) {
     BOARD_InitDebugConsole();
 
     PRINTF("Hello World\n");
-    PRINTF("Test\n");
+    PRINTF("Test again\n");
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
